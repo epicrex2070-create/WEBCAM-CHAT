874ac6d72838462ea314cb6ebb4830316d66ff58
epicrex2070-create <epicrex2070@gmail.com>
2026-09-11 21:53:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ document.getElementById("fsBtn").onclick = () => {
   const chatInput = document.getElementById("chatInput");
 const messages = document.getElementById("messages");
 </script>
+
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && chatInput.value.trim() !== "") {
+    const msg = chatInput.value.trim();
+
+    // show your own message
+    addMessage("you", msg);
+
+    // send to other person
+    ws.send(JSON.stringify({
+      chat: msg,
+      room: roomInput.value.trim()
+    }));
+
+    chatInput.value = "";
+  }
+});
